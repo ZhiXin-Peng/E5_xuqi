5399379c351c2d45bcb6bf558871c7ebc7236273
ZhiXin-Peng <1075411806@qq.com>
2025-11-05 23:04:22 +0800

    chore: initial commit

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,18 @@
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.PowerPoint) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  /**
+   * Insert your PowerPoint code here
+   */
+  const options = { coercionType: Office.CoercionType.Text };
+
+  await Office.context.document.setSelectedDataAsync(" ", options);
+  await Office.context.document.setSelectedDataAsync("hello hello", options);
+}
